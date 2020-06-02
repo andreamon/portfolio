8ace83fc5cc678af0ddea44d0b013c13f8bc97eb
Andrea Montaño <andrea14_06_2@hotmail.com>
2020-06-02 16:02:01 -0300

    Skills style modified

--- src/App.js
+++ src/App.js
@@ -61,16 +61,17 @@ const App = () => {
         <span className="special-title">Skills & Tools</span>
       </Row>
       <div id="skill">
-        <ul className="skills">
-          <li><img src="img/html5.png" height="60" alt="html5" /></li>
-          <li><img src="img/css.png" height="60" alt="css" /></li>
-          <li><img src="img/bootstrap.png" height="50" alt="bootstrap" /></li>
-          <li><img src="img/laravel.png" height="50" alt="laravel" /></li>
-          <li><img src="img/vue.png" height="50" alt="vue" /></li>
-          <li><img src="img/react.png" height="50" alt="react" /></li>
-          <li><img src="img/git.png" height="60" alt="git" /></li>
-          <li><img src="img/trello.png" height="60" alt="trello" /></li>
-        </ul>
+        <div className="skills">
+          <span><img src="img/html5.png" height="60" alt="html5" /></span>
+          <span><img src="img/css.png" height="60" alt="css" /></span>
+          <span><img src="img/javascript.png" height="60" alt="javascript" /></span>
+          <span><img src="img/bootstrap.png" height="50" alt="bootstrap" /></span>
+          <span><img src="img/laravel.png" height="50" alt="laravel" /></span>
+          <span><img src="img/vue.png" height="50" alt="vue" /></span>
+          <span><img src="img/react.png" height="50" alt="react" /></span>
+          <span><img src="img/git.png" height="60" alt="git" /></span>
+          <span><img src="img/trello.png" height="60" alt="trello" /></span>
+        </div>
       </div>
     </Container>
     <Container fluid id="contacto">
