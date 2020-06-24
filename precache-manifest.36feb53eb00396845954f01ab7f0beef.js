7dca51d4e3fa86f55b52ba719aa16c993a4832e1
Andrea Montaño <andrea14_06_2@hotmail.com>
2020-06-24 18:29:47 -0300

    Updates

---
+++ precache-manifest.36feb53eb00396845954f01ab7f0beef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7a31db399e67881e9fbc8e86bc7a7ee",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "4e513f5754fdd832e3eb",
+    "url": "/portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "0bd1258495599aa67d36",
+    "url": "/portfolio/static/css/main.c5143c68.chunk.css"
+  },
+  {
+    "revision": "4e513f5754fdd832e3eb",
+    "url": "/portfolio/static/js/2.b3c197e2.chunk.js"
+  },
+  {
+    "revision": "74c519d158df4771dcdf3b54fb80c969",
+    "url": "/portfolio/static/js/2.b3c197e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bd1258495599aa67d36",
+    "url": "/portfolio/static/js/main.04dcf597.chunk.js"
+  },
+  {
+    "revision": "88fbd3d397ae3690d5f2",
+    "url": "/portfolio/static/js/runtime-main.fb1b3dac.js"
+  }
+]);
