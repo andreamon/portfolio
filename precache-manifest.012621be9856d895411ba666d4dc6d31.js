09e73f2151a8954809564798afd5d0004f54b571
Andrea Montaño <andrea14_06_2@hotmail.com>
2020-06-25 14:59:30 -0300

    Updates

---
+++ precache-manifest.012621be9856d895411ba666d4dc6d31.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4c0cfe4c2ac53f6fd847547d26544b3",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "542ae7f8cacd1d313ed7",
+    "url": "/portfolio/static/css/3.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "10ada5382d8fb675f643",
+    "url": "/portfolio/static/css/main.c5143c68.chunk.css"
+  },
+  {
+    "revision": "1a50f3016908fbc2ea4f",
+    "url": "/portfolio/static/js/2.c6076c90.chunk.js"
+  },
+  {
+    "revision": "542ae7f8cacd1d313ed7",
+    "url": "/portfolio/static/js/3.763b1291.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/portfolio/static/js/3.763b1291.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "107dacf45746060b12e6",
+    "url": "/portfolio/static/js/4.463f1aca.chunk.js"
+  },
+  {
+    "revision": "ff6d241a7564ce6d9b04",
+    "url": "/portfolio/static/js/5.60812a97.chunk.js"
+  },
+  {
+    "revision": "b3b6e0350f61942522c0556868f2db84",
+    "url": "/portfolio/static/js/5.60812a97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac9d20c00c466e67b0a6",
+    "url": "/portfolio/static/js/6.77397a17.chunk.js"
+  },
+  {
+    "revision": "6d024ecc5b3ca9ddbd44",
+    "url": "/portfolio/static/js/7.bc7bf90c.chunk.js"
+  },
+  {
+    "revision": "8c10a6d95bbcc9179ce0",
+    "url": "/portfolio/static/js/8.da1ad99f.chunk.js"
+  },
+  {
+    "revision": "2cf140704026481acc60",
+    "url": "/portfolio/static/js/9.2a4b08db.chunk.js"
+  },
+  {
+    "revision": "10ada5382d8fb675f643",
+    "url": "/portfolio/static/js/main.d10035f3.chunk.js"
+  },
+  {
+    "revision": "d4dce72fa6c0c7f74b6d",
+    "url": "/portfolio/static/js/runtime-main.3fe14f0d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.f75c4dc6869733ca5c45ed08eb6c4616.js"
+  "/portfolio/precache-manifest.012621be9856d895411ba666d4dc6d31.js"
 );
 
 self.addEventListener('message', (event) => {
