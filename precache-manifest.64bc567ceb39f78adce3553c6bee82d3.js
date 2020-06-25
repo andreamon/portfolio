5d49c10be6654c342e8fd9c1bda749511498055c
Andrea Montaño <andrea14_06_2@hotmail.com>
2020-06-25 12:31:18 -0300

    Updates

--- precache-manifest.64bc567ceb39f78adce3553c6bee82d3.js
+++ precache-manifest.64bc567ceb39f78adce3553c6bee82d3.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0dd742c7e3ac106cf95bb20aa265800e",
+    "revision": "0dfd71e7a0849bbc1949721cef7895e5",
     "url": "/portfolio/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/css/2.af3c1da9.chunk.css"
   },
   {
-    "revision": "53464d8ab35ae62b8350",
+    "revision": "92e7d83242b15d84c7cf",
     "url": "/portfolio/static/css/main.c5143c68.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/js/2.b3c197e2.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "53464d8ab35ae62b8350",
-    "url": "/portfolio/static/js/main.0af3d274.chunk.js"
+    "revision": "92e7d83242b15d84c7cf",
+    "url": "/portfolio/static/js/main.240d3c07.chunk.js"
   },
   {
     "revision": "88fbd3d397ae3690d5f2",
